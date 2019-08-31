5b8cd6a1e9a766e95ff46cb710e4d74905433108
= <=>
2019-08-31 11:37:05 +0300

    task7

---
+++ js/main.js
@@ -0,0 +1,31 @@
+let btn = document.getElementById('start'),
+    budgetValue = document.getElementsByClassName('budget-value')[0],
+    dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+    levelValue = document.getElementsByClassName('level-value')[0],
+    expensesValue = document.getElementsByClassName('expenses-value')[0],
+    optionalExpencesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+    incomeValue = document.getElementsByClassName('income-value')[0],
+    monthsavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+    yearsavingsValue = document.getElementsByClassName('yearsavings-value')[0];
+
+let expensesItem = document.getElementsByClassName('expenses-item');
+
+let optionalExpensesBtn = document.getElementsByTagName('button')[1],
+    countBtn = document.getElementsByTagName('button')[2];
+
+let optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item');
+
+let income = document.querySelector('.choose-income'),
+    savings = document.querySelector('#savings'),
+    sum = document.querySelector('#sum'),
+    percent = document.querySelector('#percent'),
+    year = document.querySelector('.year-value'),
+    month = document.querySelector('month-value'),
+    day = document.querySelector('.day-value');
+
+ 
+
+
+
+
+
